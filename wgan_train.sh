621218106f2ec2ba737f62fcf1b7881a923d2232
Frederik <lilbahrt@gmail.com>
2020-05-03 16:57:15 +0200

    training script for all model

---
+++ wgan_train.sh
@@ -0,0 +1,4 @@
+
+python train.py --backbone wganlp --datadir ../../data/celeba
+python train.py --backbone l2_wgan --datadir ../../data/celeba
+python train.py --backbone watson_wgan --datadir ../../data/celeba
